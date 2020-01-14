7fa5f1b002158a6c23fca1c4a3fd14e7a6f9411c
devuser <santosh.behera@capgemini.com>
2020-01-15 04:04:14 +0530

    added constant file

---
+++ src/main/java/com/test/game/GameConstant.java
@@ -0,0 +1,19 @@
+package com.test.game;
+
+import java.awt.Color;
+
+/**
+ * 
+ * This class defines the constants 
+ * 
+ * 
+ *
+ */
+
+public class GameConstant {
+	
+	public static final String PANEL_NAME = "GAME_PANEL";
+	
+	
+
+}
