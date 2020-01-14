3871e946b65097734016402e54f22f567e4f10d3
devuser <santosh.behera@capgemini.com>
2020-01-15 00:26:24 +0530

    create tictacframe class

---
+++ src/test/java/tictacgame/TicTacToeTest.java
@@ -0,0 +1,23 @@
+package tictacgame;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.test.game.TicTacToeFrame;
+
+public class TicTacToeTest {
+	
+	static TicTacToeFrame frame = null;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		frame = new TicTacToeFrame();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+
+}
