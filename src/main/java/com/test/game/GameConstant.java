2420efb8772af85c97a509883bd73dbb7264d1a7
devuser <santosh.behera@capgemini.com>
2020-01-15 05:00:34 +0530

    added constant for panel, button logic

--- src/main/java/com/test/game/GameConstant.java
+++ src/main/java/com/test/game/GameConstant.java
@@ -1,19 +1,19 @@
 package com.test.game;
 
-import java.awt.Color;
-
 /**
  * 
- * This class defines the constants 
+ * This class defines the constants
  * 
  * 
  *
  */
 
 public class GameConstant {
-	
+
 	public static final String PANEL_NAME = "GAME_PANEL";
-	
-	
+
+	public static final String PLAYED = "PLAYED";
+
+	public static final String BUTTON_ADD_MESSAGE = "The grid size of tic tac game is 3X3.";
 
 }
