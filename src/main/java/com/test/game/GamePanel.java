4ff1b610f85ad80127edece95b1d79dbbeeaf59c
devuser <santosh.behera@capgemini.com>
2020-01-15 06:22:23 +0530

    added action listner class

--- src/main/java/com/test/game/GamePanel.java
+++ src/main/java/com/test/game/GamePanel.java
@@ -9,11 +9,12 @@ import javax.swing.JPanel;
 
 import com.test.game.component.GameDart;
 import com.test.game.component.GameDartImpl;
+import com.tictactoe.game.constants.GameConstant;
+import com.tictactoe.game.eventhandle.PlayButtonListener;
 
 /**
  * 
- * It's panel class which add buttons to the
- * frame.
+ * It's panel class which add buttons to the frame.
  *
  */
 
@@ -22,6 +23,9 @@ public class GamePanel extends JPanel {
 	// holds 9 play button reference of game board
 
 	public List<GameDart> playDartList = null;
+
+	// event listener for play button
+	public PlayButtonListener buttonListener = null;
 
 	public GamePanel() {
 		setLayout(new GridLayout(3, 3));
@@ -33,10 +37,10 @@ public class GamePanel extends JPanel {
 	 * This method adds 9 play button to the panel.
 	 * 
 	 *
-	 * 
 	 */
 	public void addButtonToPanel() {
 		playDartList = new ArrayList<GameDart>();
+		buttonListener = new PlayButtonListener();
 
 		for (int i = 0; i < 3; i++)
 			for (int j = 0; j < 3; j++) {
@@ -46,6 +50,8 @@ public class GamePanel extends JPanel {
 				// first time.This value will change with "X"/ "O" when the user clicks the
 				// button.
 				compButton.setCustomProperty(GameConstant.PLAYED, Math.random());
+				
+				compButton.addActionListener(buttonListener);
 
 				compButton.setBackground(Color.white);
 				add(compButton);
@@ -53,8 +59,5 @@ public class GamePanel extends JPanel {
 			}
 
 	}
-	
-	
-	
 
 }
